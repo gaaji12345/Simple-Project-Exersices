5f97aac5f0fcda060fe655076ae434ce9b0c1d44
gaji12345 <gajimax12345@gmail.com>
2024-04-30 15:59:20 +0530

    emoji changer8

---
+++ Test/js_9/js.js
@@ -0,0 +1,8 @@
+const  btn=document.querySelector('#emoji')
+
+
+const emojis=["😇","😝","😎","🤩","🥳","🥺","😩"];
+
+btn.addEventListener('mouseover',()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+})
